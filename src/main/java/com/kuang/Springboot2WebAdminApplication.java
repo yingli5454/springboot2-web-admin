768e61a342c5d8277dda136fbb04f6330b833194
yuan <yuan@123456.com>
2022-09-28 07:39:02 +0800

    1111

---
+++ src/main/java/com/kuang/Springboot2WebAdminApplication.java
@@ -0,0 +1,13 @@
+package com.kuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2WebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2WebAdminApplication.class, args);
+    }
+
+}
