87f0df6f659a17daf6d76c3c65b5e8c9a2152c7e
yuan <yuan@123456.com>
2022-09-28 08:40:21 +0800

    master

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello word1");
+        System.out.println("master text");
     }
 }
