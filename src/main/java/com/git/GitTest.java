a73229f47492a13d8b694c1d8ac1fdc98142c31e
yuan <yuan@123456.com>
2022-09-28 08:31:39 +0800

    hot-fix1

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -3,5 +3,6 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello word1");
     }
 }
